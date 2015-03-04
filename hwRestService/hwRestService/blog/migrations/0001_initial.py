a9d63b2806f3cd689d9b0431115d2f2ae194ee04
just-mura <muratbek.agabyl@ce.sdu.edu.kz>
2015-03-04 23:31:48 +0600

    Created two models and migrated them

---
+++ hwRestService/hwRestService/blog/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=30)),
+                ('text', models.CharField(max_length=100)),
+                ('pub_date', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=30)),
+                ('title', models.CharField(max_length=30)),
+                ('text', models.CharField(max_length=100)),
+                ('pub_date', models.DateTimeField(auto_now_add=True)),
+                ('upd_date', models.DateTimeField(auto_now_add=True)),
+                ('is_public', models.BooleanField(default=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='blog.Post'),
+            preserve_default=True,
+        ),
+    ]
--- hwRestService/hwRestService/blog/models.py
+++ hwRestService/hwRestService/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+	author    = models.CharField(max_length = 30)
+	title     = models.CharField(max_length = 30)
+	text      = models.CharField(max_length = 100)
+	pub_date  = models.DateTimeField(auto_now_add = timezone.now)
+	upd_date  = models.DateTimeField(auto_now_add = timezone.now)
+	is_public = models.BooleanField(default = False)
+
+class Comment(models.Model):
+	author   = models.CharField(max_length = 30)
+	text     = models.CharField(max_length = 100)
+	pub_date = models.DateTimeField(auto_now_add = timezone.now)
+	post     = models.ForeignKey(Post)	
